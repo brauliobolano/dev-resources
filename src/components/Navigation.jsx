bc3dea7b0d6ded8ba9aa6505732fcb43ce87f6e6
Braulio_Bolano <brauliobolanostrujillo@gmail.com>
2023-12-31 07:05:46 -0500

    Navigation bar

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+
+const Navigation = () => {
+    return (
+        <div>
+            <div className="bg-indigo-500">
+                <ul className="content-normal inline-flex">
+                    <li>Home</li>
+                    <li>Tutorials</li>
+                    <li>Resources</li>
+                    <li>Mentoring</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Navigation;
