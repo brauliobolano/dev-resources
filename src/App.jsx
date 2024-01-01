e52025d9ced7e162670473434a82074bda079c07
Braulio_Bolano <brauliobolanostrujillo@gmail.com>
2024-01-01 16:50:41 -0500

    Implemented Tailwind CSS setup and configured default font family

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Navigation from './components/Navigation.jsx'
 import Footer from './components/Footer.jsx'
+import TutorialBlock from './components/Tutorials/TotorialBlock.jsx'
 
 function App() {
   return (
     <>
-      <div className="App">
+      <div className="App px-12">
         <Navigation className="App-navigation"/>
+        <TutorialBlock className="App-tutorialBlock"/>
         <Footer className="App-footer"/>
       </div>
     </>
---
+++ src/components/Tutorials/TotorialBlock.jsx
@@ -0,0 +1,62 @@
+const TutorialBlock = () => {
+    return (
+        <>
+        <div className="bg-red-200">
+            <div className="TutorialBlock ">
+                <h2 className="pb-5">TutorialBlock</h2>
+            </div>
+            <hr className="border-black border-1 mb-8"/>
+            <div className="Blocks grid gap-4 grid-rows-3 grid-flow-col mb-8 ">
+                <div className="Block border-black border-4 px-5 py-2.5 bg-red-100 row-span-3">
+                    <div className="">
+                        <div><img src="https://images.ctfassets.net/aq13lwl6616q/1nGrrU8Es5woaV6siQ8X0r/b6dd4d950dae2a14cf1ae4a3f4877e56/Cheatsheet_-_Python.jpg?w=800&h=450&q=100&fm=webp&bg=transparent"/></div>
+                        <div>
+                            <h1>Heading</h1>
+                            <p>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor</p>
+                            <a href="#">Link</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="Block border-black border-4 px-5 py-2.5 bg-red-300">
+                    <div className="flex">
+                        <div><img src="https://images.ctfassets.net/aq13lwl6616q/1nGrrU8Es5woaV6siQ8X0r/b6dd4d950dae2a14cf1ae4a3f4877e56/Cheatsheet_-_Python.jpg?w=800&h=450&q=100&fm=webp&bg=transparent"/></div>
+                        <div>
+                            <h1>Heading</h1>
+                            <p>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor</p>
+                            <a href="#">Link</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="Block border-black border-4 px-5 py-2.5 bg-red-400">
+                    <div className="flex">
+                        <div><img src="https://images.ctfassets.net/aq13lwl6616q/1nGrrU8Es5woaV6siQ8X0r/b6dd4d950dae2a14cf1ae4a3f4877e56/Cheatsheet_-_Python.jpg?w=800&h=450&q=100&fm=webp&bg=transparent"/></div>
+                        <div>
+                            <h1>Heading</h1>
+                            <p>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor</p>
+                            <a href="#">Link</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="Block border-black border-4 px-5 py-2.5 bg-red-500">
+                    <div className="flex">
+                        <div><img src="https://images.ctfassets.net/aq13lwl6616q/1nGrrU8Es5woaV6siQ8X0r/b6dd4d950dae2a14cf1ae4a3f4877e56/Cheatsheet_-_Python.jpg?w=800&h=450&q=100&fm=webp&bg=transparent"/></div>
+                        <div>
+                            <h1>Heading</h1>
+                            <p>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor</p>
+                            <a href="#">Link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="LoadMore flex justify-center mb-4"><button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+Pink to orange
+</span>
+</button></div>
+        </div>
+
+        </>
+    )
+}
+
+export default TutorialBlock;
