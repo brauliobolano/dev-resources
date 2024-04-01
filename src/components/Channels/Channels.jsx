b24f16af173f48c93edfb11f4bce34a10f57c342
Braulio_Bolano <brauliobolanostrujillo@gmail.com>
2024-04-01 18:37:20 -0400

    Fixed error

--- src/components/Channels/Channels.jsx
+++ src/components/Channels/Channels.jsx
@@ -114,7 +114,7 @@ const Channels = () => {
                     </a>
                     {/* Block Hidden Channels Three */}
                     <a href="https://www.youtube.com/@BroCodez" className={`flex ${hiddenChannel} ${hiddenChannel2} lg:grid lg:grid-cols-4 flex-col items-center bg-white border border-gray-200 rounded-lg shadow lg:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 shadow-xl shadow-inner`}>
-                        <div className={`flex xl:h-auto w-full md:w-full lg:w-auto lg:h-full xl:h-full bg-custom-image items-center justify-center`}> <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-[170.66px] md:rounded-none md:rounded-s-lg" src={BroCodeImage} alt=""/></div>
+                        <div className={`flex xl:h-auto w-full md:w-full lg:w-auto lg:h-full xl:h-full items-center justify-center`}> <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-[170.66px] md:rounded-none md:rounded-s-lg" src={BroCodeImage} alt=""/></div>
                             <div className="col-span-3 flex flex-col justify-between p-4 leading-normal"> 
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Bro Code</h5>
                                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
